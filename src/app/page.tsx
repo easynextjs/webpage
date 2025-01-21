f09d62296220fd7ee3e1db7dd15c54a2fadc9810
greatSumini <greatSumini@gmail.com>
2025-01-21 11:34:57 +0900

    feat: add home page (temp)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,103 @@
-import Image from "next/image";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { CheckCircle, Copy, Github } from "lucide-react";
+import React, { useState } from "react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="min-h-screen flex">
+      <div className="flex flex-col p-5 md:p-8 space-y-4">
+        <h1 className="text-3xl md:text-5xl font-semibold tracking-tighter !leading-tight text-left">
+          Easiest way to start
+          <br /> Next.js project
+          <br /> with Cursor
+        </h1>
+        <p className="text-lg text-muted-foreground">
+          Get Highest quality template for your Next.js project
+        </p>
+        <div className="flex items-center gap-2">
+          <Button
+            variant="secondary"
+            size="lg"
+            className="gap-2 w-fit rounded-full px-5 border border-black"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            npm i -g @easynext/cli <Copy className="w-4 h-4" />
+          </Button>
+          <Button
+            asChild
+            size="lg"
+            className="gap-2 w-fit rounded-full px-5 border border-black"
           >
-            Read our docs
-          </a>
+            <a href="https://github.com/easynextjs">
+              <Github className="w-4 h-4" />
+              GitHub
+            </a>
+          </Button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <Section />
+      </div>
     </div>
+  );
+}
+
+function Section() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const items = [
+    { href: "https://nextjs.org/", label: "Next.js" },
+    { href: "https://ui.shadcn.com/", label: "shadcn/ui" },
+    { href: "https://tailwindcss.com/", label: "Tailwind CSS" },
+    { href: "https://www.framer.com/motion/", label: "framer-motion" },
+    { href: "https://zod.dev/", label: "zod" },
+    { href: "https://date-fns.org/", label: "date-fns" },
+    { href: "https://ts-pattern.dev/", label: "ts-pattern" },
+    { href: "https://es-toolkit.dev/", label: "es-toolkit" },
+    { href: "https://zustand.docs.pmnd.rs/", label: "zustand" },
+    { href: "https://supabase.com/", label: "supabase" },
+    { href: "https://react-hook-form.com/", label: "react-hook-form" },
+  ];
+
+  const visibleItems = isExpanded ? items : items.slice(0, 5);
+
+  return (
+    <div className="flex flex-col py-5 md:py-8 space-y-2 opacity-75">
+      <p className="font-semibold">What&apos;s Included</p>
+      <div className="flex flex-col space-y-1 text-muted-foreground">
+        {visibleItems.map((item) => (
+          <SectionItem key={item.href} href={item.href}>
+            {item.label}
+          </SectionItem>
+        ))}
+        {!isExpanded && items.length > 5 && (
+          <Button
+            variant="ghost"
+            className="w-fit text-muted-foreground hover:text-foreground"
+            onClick={() => setIsExpanded(true)}
+          >
+            + {items.length - 5} more
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+function SectionItem({
+  children,
+  href,
+}: {
+  children: React.ReactNode;
+  href: string;
+}) {
+  return (
+    <a
+      href={href}
+      className="flex items-center gap-2 underline"
+      target="_blank"
+    >
+      <CheckCircle className="w-4 h-4" />
+      <p>{children}</p>
+    </a>
   );
 }
